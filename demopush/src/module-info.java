0adeb2abce8813465241096fb5769ec97879c108
yamini-97 <yaminikuncha@gmail.com>
2023-10-30 11:21:26 +0530

    first commit

---
+++ demopush/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author K.Yamini
+ *
+ */
+module demopush {
+}
---
+++ demopush/src/pack1/Manu.java
@@ -0,0 +1,10 @@
+package pack1;
+
+public class Manu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Welcome to GPREC");
+	}
+
+}
